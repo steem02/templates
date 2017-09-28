8ffcc5c1ba556d53eba3f1c541eb0580b5b5e437
Steem <svsteem@gmail.com>
2017-09-28 15:44:36 +0000

    slide carousel stage4

--- app/js/first.js
+++ app/js/first.js
@@ -7,6 +7,7 @@ mediaPhone.addListener(activePhone480);
 function activePhone480(media) {
 
     if (mediaPhone.matches) {
+        container.firstElementChild.classList.add('js__slide');
         function SliderPhone(img, container) {
             this.picture = img;
             this.container = container;
@@ -18,7 +19,7 @@ function activePhone480(media) {
             if (this._LENGTH >= 3) {
                 this.picture[0].classList.add('js__img_left');
                 this.picture[2].classList.add('js__img_right');
-                if (this._LENGTH = 4) this.picture[3].style.zIndex = '-999';
+                if (this._LENGTH = 4) this.picture[3].style.display = 'none';
             } else {
                 return;
             };
@@ -27,18 +28,43 @@ function activePhone480(media) {
         SliderPhone.prototype.touchMove = function () {
             var count = this.count;
             var img = this.picture;
-          
+            if (count < 0) {
+                count = img.length - count;
+                this.count = count;
+            };
+            if (count >= img.length) {
+                count = count - img.length;
+                this.count = count;
+            };
             function swipeLeft(num) {
-                img[num+2].style.display = 'none';
-                img[num-1].style.display = 'none';
-                img[num].classList.add('js__img_transition');
-                img[num+1].classList.add('js__img_transition');
+                var under = num + 2;
+                var right = under - 1;
+                var left = num - 1;
                 
-                img[num-1].classList.remove('js__img_left');
-                img[num+1].classList.remove('js__img_right');
+                if (under >= img.length) {
+                    under = under - img.length;
+                    
+                    if (under == 0) {
+                        right = img.length - 1;
+                    } else {
+                        right = 0;    
+                    };
+                    
+                };
+                
+                if (num == 0) {
+                    left = img.length - 1;
+                };
+                              
+              
+                
+                // img[under].style.display = 'none';
+                // img[left].style.display = 'none';
+                img[left].classList.remove('js__img_left');
+                img[right].classList.remove('js__img_right');
                 img[num].classList.add('js__img_left');
-                img[num+2].classList.add('js__img_right');
-                img[num+2].style.display = 'inline-block';
+                img[under].classList.add('js__img_right');
+                img[under].style.display = 'inline-block';
                 count++;
             };
             function swipeRight(num) {
@@ -53,15 +79,18 @@ function activePhone480(media) {
                 count--;
             };
             
-            swipeLeft(count);
+            swipeLeft(this.count);
+            this.count = count;
         };
         var sld = new SliderPhone(img, container);
         sld.begin();
         container.onclick = function() {sld.touchMove()};
     } else {
         container.onclick = '';
+        container.firstElementChild.classList.remove('js__slide');
         for (var i = 0; i < img.length; i++) {
-            img[i].style.transform = '';
+            img[i].classList.remove('js__img_left');
+            img[i].classList.remove('js__img_right');
             img[i].style.zIndex = '';
             img[i].style.display = 'inline-block';
         }
