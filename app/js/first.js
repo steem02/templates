457ab6ceba2f802b20009ba2df899b65468dd5b8
Steem <svsteem@gmail.com>
2017-09-29 21:48:57 +0000

    phone slide finish stage4

--- app/js/first.js
+++ app/js/first.js
@@ -1,6 +1,7 @@
 var mediaPhone = window.matchMedia('only screen and (max-width: 480px)');
 var img = document.getElementsByClassName('js__tastes-photo');
 var container = document.getElementsByClassName('js__slide-contain')[0];
+var eventSliderTouchStart, eventSliderTouchEnd;
 activePhone480(mediaPhone);
 mediaPhone.addListener(activePhone480);
 
@@ -12,7 +13,6 @@ function activePhone480(media) {
             this.picture = img;
             this.container = container;
             this._LENGTH = this.picture.length;
-            this.count = 1;
         };
         SliderPhone.prototype.begin = function () {
                 
@@ -26,17 +26,17 @@ function activePhone480(media) {
         
         };
         SliderPhone.prototype.touchMove = function () {
-            var count = this.count;
+            var count = 1;
             var img = this.picture;
-            if (count < 0) {
-                count = img.length - count;
-                this.count = count;
-            };
-            if (count >= img.length) {
-                count = count - img.length;
-                this.count = count;
-            };
-            function swipeLeft(num) {
+            function getIndexPictures(num) {
+                
+                if (num < 0) {
+                    num = img.length + num;
+                };
+                if (num >= img.length) {
+                    num = num - img.length;
+                };
+                
                 var under = num + 2;
                 var right = under - 1;
                 var left = num - 1;
@@ -55,43 +55,77 @@ function activePhone480(media) {
                 if (num == 0) {
                     left = img.length - 1;
                 };
-                              
-              
-                
-                // img[under].style.display = 'none';
-                // img[left].style.display = 'none';
-                img[left].classList.remove('js__img_left');
-                img[right].classList.remove('js__img_right');
-                img[num].classList.add('js__img_left');
-                img[under].classList.add('js__img_right');
-                img[under].style.display = 'inline-block';
+                return {left:left,right:right,under:under,count:num};
+            };
+            function swipeLeft(num) {
+                var obj = getIndexPictures(num);
+                img[obj.under].style.display = 'none';
+                img[obj.left].style.display = 'none';
+                img[obj.left].classList.remove('js__img_left');
+                img[obj.right].classList.remove('js__img_right');
+                img[obj.count].classList.add('js__img_left');
+                img[obj.under].classList.add('js__img_right');
+                img[obj.under].style.display = 'inline-block';
+                count = obj.count;
                 count++;
             };
             function swipeRight(num) {
-                img[num+1].style.zIndex = '-1';
-                img[num+1].classList.remove('js__img_right');
-                img[num].classList.add('js__img_right');
-                img[num-1].classList.remove('js__img_left');
-                img[num-2].classList.add('js__img_left');
-                setTimeout(function () {
-                    img[num-2].style.zIndex = '999';
-                }, 400);
+                var obj = getIndexPictures(num);
+                img[obj.under].style.display = 'none';
+                img[obj.right].style.display = 'none';
+                img[obj.left].classList.remove('js__img_left');
+                img[obj.right].classList.remove('js__img_right');
+                img[obj.count].classList.add('js__img_right');
+                img[obj.under].classList.add('js__img_left');
+                img[obj.under].style.display = 'inline-block';
+                count = obj.count;
                 count--;
             };
+            //Move
+            var initialPoint;
+            var finalPoint;
+            eventSliderTouchStart = function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                initialPoint = event.changedTouches[0];
+            };
+            eventSliderTouchEnd = function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                finalPoint=event.changedTouches[0];
+                var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+              
+                if (xAbs > 5) {
+                    
+                    if (finalPoint.pageX < initialPoint.pageX){
+                        swipeLeft(count);
+                        
+                    } else {
+                        swipeRight(count);
+                    };
+                     
+                };
             
-            swipeLeft(this.count);
-            this.count = count;
+            };
+            this.container.addEventListener('touchstart',eventSliderTouchStart, false);
+            this.container.addEventListener('touchend',eventSliderTouchEnd, false);
+            if (!document.ontouchstart || !document.ontouchend) {
+                this.container.onclick = function () {
+                    swipeLeft(count);
+                };
+            };
         };
         var sld = new SliderPhone(img, container);
         sld.begin();
-        container.onclick = function() {sld.touchMove()};
+        sld.touchMove();
     } else {
+        container.removeEventListener('touchstart', eventSliderTouchStart);
+        container.removeEventListener('touchend', eventSliderTouchEnd);
         container.onclick = '';
         container.firstElementChild.classList.remove('js__slide');
         for (var i = 0; i < img.length; i++) {
             img[i].classList.remove('js__img_left');
             img[i].classList.remove('js__img_right');
-            img[i].style.zIndex = '';
             img[i].style.display = 'inline-block';
         }
     }
