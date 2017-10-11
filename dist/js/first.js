94de64f8a94b1e1543bb60ac07a82d95a0807175
Steem <svsteem@gmail.com>
2017-10-11 22:33:53 +0000

    click events button  hide show menu

--- dist/js/first.js
+++ dist/js/first.js
@@ -2,11 +2,13 @@ if (window.matchMedia) {
     var mediaPhone = window.matchMedia('only screen and (max-width: 480px)');
     var img = document.getElementsByClassName('js__tastes-photo');
     var container = document.getElementsByClassName('js__slide-contain')[0];
-    var eventSliderTouchStart, eventSliderTouchEnd, eventSliderScroll;
     var pancakesIcon = document.getElementsByClassName('pancakes__icon');
     var sandwichIcon = document.getElementsByClassName('sandwich__icon');
     var sandwichIconTitle = document.getElementsByClassName('sandwich__icontitle');
     var sandwichIconText = document.getElementsByClassName('sandwich__icontext');
+    var sld;
+    var naviButton = document.getElementsByClassName('navi__button')[0];
+    var headerNavi = document.getElementsByClassName('header__navi')[0];
     activePhone480(mediaPhone);
     mediaPhone.addListener(activePhone480);
     
@@ -14,11 +16,12 @@ if (window.matchMedia) {
     
         if (mediaPhone.matches) {
             container.firstElementChild.classList.add('js__slide');
-            function SliderPhone(img, container) {
+            function SliderPhone(img, container, media) {
                 this.picture = img;
                 this.container = container;
                 this._LENGTH = this.picture.length;
-                this._COUNT = 2; 
+                this._COUNT = 1;
+                this.media = media || false;
             };
             SliderPhone.prototype.getIndexPictures = function (num) {
                     
@@ -58,7 +61,8 @@ if (window.matchMedia) {
                 img[obj.count].classList.add('js__img_left');
                 img[obj.under].classList.add('js__img_right');
                 img[obj.under].style.display = 'inline-block';
-                obj.count++
+                obj.count++;
+                this._COUNT = this.getIndexPictures(obj.count).count;
                 return obj.count;
             };
             SliderPhone.prototype.swipeRight = function (num) {
@@ -71,6 +75,7 @@ if (window.matchMedia) {
                 img[obj.under].classList.add('js__img_left');
                 img[obj.under].style.display = 'inline-block';
                 obj.count--;
+                this._COUNT = this.getIndexPictures(obj.count).count;
                 return obj.count;
             };
             SliderPhone.prototype.begin = function () {
@@ -78,25 +83,26 @@ if (window.matchMedia) {
                 if (this._LENGTH >= 3) {
                     this.picture[0].classList.add('js__img_left');
                     this.picture[2].classList.add('js__img_right');
-                    if (this._LENGTH = 4) this.picture[3].style.display = 'none';
+                    if (this._LENGTH == 4) this.picture[3].style.display = 'none';
                 } else {
                     return;
                 };
-            
+                
             };
             SliderPhone.prototype.touchMove = function () {
                 var count = this._COUNT;
                 var img = this.picture;
+                var container = this.container;
+                var media = this.media;
                 var initialPoint;
                 var finalPoint;
                 var swipeLeft = this.swipeLeft.bind(this);
                 var swipeRight = this.swipeRight.bind(this);
-                eventSliderTouchStart = function(event) {
-                    event.preventDefault();
+                function start(event) {
                     event.stopPropagation();
                     initialPoint = event.changedTouches[0];
                 };
-                eventSliderTouchEnd = function(event) {
+                function end(event) {
                     event.preventDefault();
                     event.stopPropagation();
                     finalPoint = event.changedTouches[0];
@@ -116,21 +122,30 @@ if (window.matchMedia) {
                     };
                 
                 };
-                this.container.addEventListener('touchstart',eventSliderTouchStart, false);
-                this.container.addEventListener('touchend',eventSliderTouchEnd, false);
+                this.begin();
+                this.container.addEventListener('touchstart', start, false);
+                this.container.addEventListener('touchend', end, false);
                 if (!document.ontouchstart || !document.ontouchend) {
-                    this.container.onclick = function () {
+                    function tap() {
                        var a = swipeLeft(count);
                        count = a;
                     };
+                    this.container.addEventListener('click', tap);
+                };
+                if (media) {
+                    media.addListener(function () {
+                        container.removeEventListener('touchstart', start);
+                        container.removeEventListener('touchend', end);
+                        container.removeEventListener('click', tap);
+                    })
                 };
             };
             SliderPhone.prototype.scrollVisible = function () {
                 var container = this.container;
                 var swipeLeft = this.swipeLeft.bind(this);
+                var media = this.media;
                 var img = this.picture;
-    
-                eventSliderScroll = function (e) {
+                function eventScroll(e) {
                     var winHeight = document.body.clientHeight;
                     var sliderCoord = container.getBoundingClientRect();
                     
@@ -147,16 +162,15 @@ if (window.matchMedia) {
                         setTimeout(function () {
                             swipeLeft(1);
                         }, getTimeTransition());
-                        window.removeEventListener('scroll', eventSliderScroll);
+                        window.removeEventListener('scroll', eventScroll);
                     };
     
                 };
-                window.addEventListener('scroll', eventSliderScroll);
+                window.addEventListener('scroll', eventScroll);
             };
-            var sld = new SliderPhone(img, container);
-            sld.begin();
-            sld.touchMove();
+            sld = new SliderPhone(img, container, mediaPhone);
             sld.scrollVisible();
+            sld.touchMove();
             
             // pancakes        
             
@@ -167,7 +181,7 @@ if (window.matchMedia) {
             };
             setClassIcon(pancakesIcon);
             
-            //sandwich
+            // sandwich
             
             function startSandwichAnimate() {
                 var count = 0;
@@ -192,20 +206,128 @@ if (window.matchMedia) {
                     sandwichIcon[count].classList.remove('js__sandwich_none');
                     sandwichIconTitle[count].classList.remove('js__sandwich__title_left');
                     sandwichIconText[count].classList.remove('js__sandwich__text_right');
-                    setTimeout(thisFunc, 4000);
+                    sandwichTimeout = setTimeout(thisFunc, 4000);
                 };
                 
                 setTimeout(thisFunc, 4000);
             };
             startSandwichAnimate();
+            
+            // menu
+            
+            function Menu(outer, inner, media) {
+                this.button = outer;
+                this.list = inner;
+                this.state = false;
+                this.media = media || false;
+                this.click = function () {
+                    var state = this.state;
+                    var list = this.list;
+                    var button = this.button;
+                    var media = this.media;
+                    var backgroundElem = createBackground();
+                    document.body.appendChild(backgroundElem);
+                    setScrollNone();
+                    function createBackground () {
+                        var elem = document.createElement('div');
+                        elem.classList.add('navi__background');
+                        return elem;
+                    }
+                    function setScrollNone() {
+                        function stop(e) {
+                            e.preventDefault();
+                        };
+                        list.addEventListener('touchmove', stop);
+                        if (media) { 
+                            media.addListener(function () {
+                                list.removeEventListener('touchmove', stop);    
+                            });
+                        };
+                    };
+                    function getTimeTransition () {
+                            var reg = /\d\.?\d?/;
+                            var duration = +getComputedStyle(list).transitionDuration.match(reg)[0];
+                            return duration*1000;
+                        };
+                    function downButtonAnimate(e) {
+                        e.stopPropagation();
+                        if(e.target == this) {
+                            button.style.boxShadow = 'none';
+                            button.style.transform = 'scale(.9)';
+                        };
+                        
+                    };
+                    function upButtonAnimate(e) {
+                        e.stopPropagation();
+                        if(e.target == this) {
+                            button.style.boxShadow = '';
+                            button.style.transform = '';
+                        };
+                    };
+                    function hideMenu(e) {
+                        backgroundElem.style.background = '';
+                        setTimeout(function () {
+                            backgroundElem.style.transition = '';
+                        }, 1000);
+                        backgroundElem.style.transform = '';
+                        backgroundElem.removeEventListener('touchstart', hideTouchBackground);
+                        list.classList.add('js__navi_min');
+                        list.classList.remove('js__navi_show');
+                        setTimeout(function () {
+                          list.classList.remove('js__navi_min');  
+                        }, getTimeTransition());
+                        state = false;
+                    };
+                    function showMenu(e) {
+                        backgroundElem.style.transform = 'scale(1)';
+                        backgroundElem.style.transition = 'transition all 1s ease';
+                        backgroundElem.style.background = '#000';
+                        backgroundElem.addEventListener('touchstart', hideTouchBackground);
+                        list.classList.add('js__navi_max');
+                        list.classList.add('js__navi_show');
+                        setTimeout(function () {
+                          list.classList.remove('js__navi_max');  
+                        }, getTimeTransition());
+                        state = true;
+                    };
+                    function hideTouchBackground(e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        if (e.target == this) {
+                            hideMenu();
+                        };
+                    };
+                    function eventClick (e) {
+                        e.stopPropagation();
+                        if (e.target == this) {
+                            if (state) {
+                                hideMenu();
+                            } else {
+                                showMenu();
+                            };
+                        };
+                    };
+                    this.button.addEventListener('touchstart', downButtonAnimate);
+                    this.button.addEventListener('touchend', upButtonAnimate);
+                    this.button.addEventListener('click', eventClick);
+                    if (media) { 
+                        media.addListener(function () {
+                            button.removeEventListener('click', eventClick);
+                            button.removeEventListener('mousedown', downButton);
+                            button.removeEventListener('mouseup', upButton);
+                            backgroundElem.removeEventListener('touchstart', hideTouchBackground);
+                            document.body.removeChild(backgroundElem);
+                        });
+                    };
+                };
+            };
+            var headerMenu = new Menu(naviButton, headerNavi, mediaPhone);
+            headerMenu.click();
            
         } else {
-            container.removeEventListener('touchstart', eventSliderTouchStart);
-            container.removeEventListener('touchend', eventSliderTouchEnd);
-            window.removeEventListener('scroll', eventSliderScroll);
-            container.onclick = '';
             container.firstElementChild.classList.remove('js__slide');
-          
+            headerNavi.className = 'header__navi';
+            clearTimeout(sandwichTimeout);
             for (var i = 0; i < img.length; i++) {
                 img[i].classList.remove('js__img_left');
                 img[i].classList.remove('js__img_right');
@@ -215,10 +337,10 @@ if (window.matchMedia) {
             for(var i = 0; i < sandwichIcon.length; i++) {
                 sandwichIcon[i].classList.remove('js__sandwich_none');
                 sandwichIconTitle[i].classList.remove('js__sandwich__title_left');
-                sadwichIconText[i].classList.remove('js__sandwich__text_right');
-                sandwichIcon[k].classList.remove('sandwich__transition');
-                sandwichIconTitle[k].classList.remove('sandwich__transition');
-                sandwichIconText[k].classList.remove('sandwich__transition');
+                sandwichIconText[i].classList.remove('js__sandwich__text_right');
+                sandwichIcon[i].classList.remove('sandwich__transition');
+                sandwichIconTitle[i].classList.remove('sandwich__transition');
+                sandwichIconText[i].classList.remove('sandwich__transition');
             };
         
         };
